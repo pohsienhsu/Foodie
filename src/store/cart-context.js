5196b8d18f55dc0b025d1e782e51cf9b76b1e051
phsu40 <phsu40@gatech.edu>
2023-05-06 17:07:59 -0700

    Create cart context provider component to wrap around App.js

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-React.createContext({
+const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
   addItem: (item) => {},
   removeItem: (id) => {}
 });
 
-export default CartContext
+export default CartContext;
